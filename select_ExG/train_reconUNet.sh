a37c386edf0f3f784b7f42bfceff5d0a4dddf03a
shimooo3 <undermouth0330@gmail.com>
2025-10-29 03:26:41 -0400

    add

--- select_ExG/train_reconUNet.sh
+++ select_ExG/train_reconUNet.sh
@@ -6,9 +6,10 @@ conda activate ymc
 python "../reconU.py" \
     --real_image_dir="./train/target/" \
     --composition_image_dir="./train/source/" \
-    --decoder_weights_path="../__output__/02-1_controlnet_keypoint/condition_weight/econstruction_decoder.pt" \
+    --decoder_weights_path="../__output__/02-1_controlnet_keypoint/condition_weight/20251025-0301_unetDecoder01_daytime/checkpoints/best_DREAMSIM_checkpoint/checkpoint-150_value-0.29048516/reconstruction_decoder.pt" \
     --output_dir="./output" \
     --epochs=5000 \
     --batch_size=8 \
     --lr=1e-5 \
-    # --unfreeze_decoder
+    --unfreeze_decoder_from_epoch=1000 \
+    # --use_all_region_from_epoch= \
